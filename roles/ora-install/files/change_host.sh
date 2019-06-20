44fb760dec453cc8a942588054620e2bce746734
Jakit <jakit_mail@163.com>
2019-06-21 01:40:44 +0800

    Update

--- roles/ora-install/files/change_host.sh
+++ roles/ora-install/files/change_host.sh
@@ -5,7 +5,11 @@
 ## Date: 2019/05/08
 ##
 
-if [[ -e /tmp/oracle_install_change_host.conf ]]; then
-	cp /etc/hosts /etc/hosts.bk
-	cat /tmp/oracle_install_change_host.conf >> /etc/hosts
+if [[ -e /etc/hosts.bk ]]; then
+	cp -f /etc/hosts.bk /etc/hosts
+fi
+
+if [[ -e /tmp/hosts ]]; then
+	cp -f /etc/hosts /etc/hosts.bk
+	cat /tmp/hosts >> /etc/hosts
 fi
