7e80772a3a0bd18ca533838abc7c067aa569fd54
Jakit <jakit_mail@163.com>
2019-06-21 18:35:09 +0800

    Update

--- roles/ora-master/files/prepare_database.sh
+++ roles/ora-master/files/prepare_database.sh
@@ -5,12 +5,12 @@
 ## Date: 2019/05/08
 ##
 
-/u01/app/oracle/oraInventory/orainstRoot.sh
-/u01/app/oracle/product/11.2.0/db_1/root.sh
+/oracle/app/oracle/oraInventory/orainstRoot.sh
+/oracle/app/oracle/product/11.2.0/db_1/root.sh
 
-cp /tmp/listener.ora /u01/app/oracle/product/11.2.0/db_1/network/admin/
-cp /tmp/tnsnames.ora /u01/app/oracle/product/11.2.0/db_1/network/admin/
-cp /tmp/initpri.ora /u01/app/oracle/product/11.2.0/db_1/dbs
+cp /tmp/listener.ora /oracle/app/oracle/product/11.2.0/db_1/network/admin/
+cp /tmp/tnsnames.ora /oracle/app/oracle/product/11.2.0/db_1/network/admin/
+cp /tmp/initpri.ora /oracle/app/oracle/product/11.2.0/db_1/dbs
 
 su - oracle
 
@@ -24,10 +24,10 @@ startup mount;
 alter database force logging;
 alter database archivelog;
 alter database open;
-alter database add standby logfile group 4 '/u01/app/oracle/oradata/pri/std_redo04.log' size 50m;
-alter database add standby logfile group 5 '/u01/app/oracle/oradata/pri/std_redo05.log' size 50m;
-alter database add standby logfile group 6 '/u01/app/oracle/oradata/pri/std_redo06.log' size 50m;
-alter database add standby logfile group 7 '/u01/app/oracle/oradata/pri/std_redo07.log' size 50m;
+alter database add standby logfile group 4 '/oracle/app/oracle/oradata/pri/std_redo04.log' size 50m;
+alter database add standby logfile group 5 '/oracle/app/oracle/oradata/pri/std_redo05.log' size 50m;
+alter database add standby logfile group 6 '/oracle/app/oracle/oradata/pri/std_redo06.log' size 50m;
+alter database add standby logfile group 7 '/oracle/app/oracle/oradata/pri/std_redo07.log' size 50m;
 exit;
 EOF
 
--- roles/ora-slave/files/prepare_slave_database.sh
+++ roles/ora-slave/files/prepare_slave_database.sh
@@ -5,17 +5,17 @@
 ## Date: 2019/05/08
 ##
 
-mkdir -p /u01/app/oracle/admin/std/adump
-mkdir -p /u01/app/oracle/diag/rdbms/std/std/trace
-mkdir -p /u01/app/oracle/arch
-mkdir -p /u01/app/oracle/oradata/std
-mkdir -p /u01/app/oracle/oradata/standbylog
-mkdir -p /u01/app/oracle/flash_recovery_area
+mkdir -p /oracle/app/oracle/admin/std/adump
+mkdir -p /oracle/app/oracle/diag/rdbms/std/std/trace
+mkdir -p /oracle/app/oracle/arch
+mkdir -p /oracle/app/oracle/oradata/std
+mkdir -p /oracle/app/oracle/oradata/standbylog
+mkdir -p /oracle/app/oracle/flash_recovery_area
 
-cp /tmp/listener.ora /u01/app/oracle/product/11.2.0/db_1/network/admin/
-cp /tmp/tnsnames.ora /u01/app/oracle/product/11.2.0/db_1/network/admin/
-cp /tmp/initstd.ora /u01/app/oracle/product/11.2.0/db_1/dbs
-cp /tmp/orapwstd /u01/app/oracle/product/11.2.0/db_1/dbs
+cp /tmp/listener.ora /oracle/app/oracle/product/11.2.0/db_1/network/admin/
+cp /tmp/tnsnames.ora /oracle/app/oracle/product/11.2.0/db_1/network/admin/
+cp /tmp/initstd.ora /oracle/app/oracle/product/11.2.0/db_1/dbs
+cp /tmp/orapwstd /oracle/app/oracle/product/11.2.0/db_1/dbs
 
 sleep 300
 
