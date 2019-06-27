899bf498c4ed514079f8db5f7cb51a5363644648
Jakit <jakit_mail@163.com>
2019-06-27 18:54:48 +0800

    Update

--- roles/ora-slave/files/sync_database.sh
+++ roles/ora-slave/files/sync_database.sh
@@ -16,12 +16,16 @@ startup nomount;
 exit;
 EOF
 
+echo "mount ok" >> /tmp/sync_log.log
+
 sleep 30
 
 rman target sys/system@pri auxiliary sys/system@std <<EOF
 duplicate target database for standby from active database nofilenamecheck;
 exit;
 EOF
+
+echo "duplicate ok" >> /tmp/sync_log.log
 
 sleep 120
 
@@ -31,11 +35,15 @@ startup;
 exit;
 EOF
 
+echo "restart ok" >> /tmp/sync_log.log
+
 sleep 20
 
 sqlplus -s "/ as sysdba" <<EOF
 alter database recover managed standby database disconnect from session;
 exit;
 EOF
+
+echo "recover ok" >> /tmp/sync_log.log
 
 exit
