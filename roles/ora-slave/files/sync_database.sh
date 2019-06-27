bf970843a697b889edf9719186ec8293c9e8f603
Jakit <jakit_mail@163.com>
2019-06-28 00:44:09 +0800

    Update

--- roles/ora-slave/files/sync_database.sh
+++ roles/ora-slave/files/sync_database.sh
@@ -30,6 +30,22 @@ echo "duplicate ok" >> /tmp/sync_log.log
 sleep 30
 
 sqlplus -s "/ as sysdba" <<EOF
+alter database recover managed standby database disconnect from session;
+exit;
+EOF
+
+# sqlplus -s "/ as sysdba" <<EOF
+# alter database recover managed standby database cancel;
+# alter database open read only;
+# alter database recover managed standby database disconnect from session;
+# exit;
+# EOF
+
+echo "recover ok" >> /tmp/sync_log.log
+
+sleep 5
+
+sqlplus -s "/ as sysdba" <<EOF
 shutdown immediate;
 exit;
 EOF
@@ -46,17 +62,8 @@ echo "restart ok" >> /tmp/sync_log.log
 sleep 5
 
 sqlplus -s "/ as sysdba" <<EOF
-alter database recover managed standby database disconnect from session;
+recover managed standby database using current logfile disconnect from session;
 exit;
 EOF
-
-# sqlplus -s "/ as sysdba" <<EOF
-# alter database recover managed standby database cancel;
-# alter database open read only;
-# alter database recover managed standby database disconnect from session;
-# exit;
-# EOF
-
-echo "recover ok" >> /tmp/sync_log.log
 
 exit
